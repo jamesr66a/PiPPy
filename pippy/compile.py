3a10c67b87bcb2006402c11208ae599c1e10cc24
James Reed <james@fireworks.ai>
2023-03-23 04:58:14 +0000

    deferred init hacks

--- pippy/compile.py
+++ pippy/compile.py
@@ -88,7 +88,7 @@ def _compile(
     # We can hence ask each rank to get its own stage from the pipe, and materialize it locally.
     if all_compile:
         device = get_device()
-        pipe_model.defer_stage_init(device)
+        # pipe_model.defer_stage_init(device)
         stage_mod = pipe_model.export(pp_rank)
 
     if pp_rank == 0:
